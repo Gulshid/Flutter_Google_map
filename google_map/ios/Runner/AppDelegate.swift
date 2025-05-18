961df1678378f4efcc65486adba37526231f8377
Gulshid Zada <gulshidzada4@gmail.com>
2025-05-18 22:43:43 +0500

    add location tracker icon

--- google_map/ios/Runner/AppDelegate.swift
+++ google_map/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyA45Nrq6VfED-kciNyHsCwVMA75kmaXK_E")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- google_map/lib/View_/Map_view.dart
+++ google_map/lib/View_/Map_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
   const MapView({super.key});
@@ -8,8 +10,61 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  LatLng myCurrentLocation = const LatLng(30.3753, 69.3451);
+
+  late GoogleMapController googleMapController;
+  Set<Marker> marker = {};
+  // BitmapDescriptor bitmapDescriptor = BitmapDescriptor.defaultMarker;
+  // @override
+  // void initState() {
+  //   customMarker();
+  //   super.initState();
+  // }
+
+  // void customMarker() {
+  //   BitmapDescriptor.asset(ImageConfiguration(), "assets/25530.jpg").then((
+  //     icon,
+  //   ) {
+  //     setState(() {
+  //       bitmapDescriptor = icon;
+  //     });
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        initialCameraPosition: CameraPosition(
+          target: myCurrentLocation,
+          zoom: 14,
+        ),
+        markers: marker,
+        onMapCreated: (GoogleMapController controller) {
+          googleMapController = controller;
+        },
+      
+
+        // markers: {
+        //   Marker(
+        //     markerId: MarkerId("marker id"),
+        //     position: myCurrentLocation,
+        //     onDragEnd: (value) {},
+        //     infoWindow: InfoWindow(
+        //       title: 'title of the marker',
+        //       snippet: 'This is a Snippet',
+        //     ),
+
+        //     icon: bitmapDescriptor,
+        //   ),
+        // },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.white,
+        onPressed: (){},
+        child: Icon(Icons.my_location, size: 30.sp,),),
+    );
   }
 }
