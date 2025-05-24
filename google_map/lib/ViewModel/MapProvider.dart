24eeca1eebdd18ae1837d1b8a3b075cfdace712f
Gulshid Zada <gulshidzada4@gmail.com>
2025-05-24 15:43:45 +0500

    Completed Google map Successfully------->

--- google_map/lib/ViewModel/MapProvider.dart
+++ google_map/lib/ViewModel/MapProvider.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapprovider with ChangeNotifier{
+  LatLng myCurrentLocation = const LatLng(30.3753, 69.3451);
+  Set<Marker> markers = {};
+  GoogleMapController? googleMapController;
+
+  Future<void> getCurrentLocation() async {
+    bool serviceEnable;
+    LocationPermission permission;
+    serviceEnable = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnable) {
+      throw Exception('Location services are disabled');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permission denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Location permission permanently denied');
+    }
+    Position position = await Geolocator.getCurrentPosition();
+    myCurrentLocation = LatLng(position.latitude, position.longitude);
+    markers.clear();
+    markers.add(
+      Marker(
+        markerId: MarkerId('current_location'),
+        position: myCurrentLocation,
+      ),
+    );
+    notifyListeners();
+    googleMapController?.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(target: myCurrentLocation, zoom: 14),
+      ),
+    );
+  }
+
+  void setMapController(GoogleMapController controller) {
+    googleMapController = controller;
+    notifyListeners();
+  }
   
 }
--- google_map/lib/View_/Map_view.dart
+++ google_map/lib/View_/Map_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:google_map/ViewModel/MapProvider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 class MapView extends StatefulWidget {
   const MapView({super.key});
@@ -11,10 +12,6 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  LatLng myCurrentLocation = const LatLng(30.3753, 69.3451);
-
-  late GoogleMapController googleMapController;
-  Set<Marker> marker = {};
   // BitmapDescriptor bitmapDescriptor = BitmapDescriptor.defaultMarker;
   // @override
   // void initState() {
@@ -32,42 +29,19 @@ class _MapViewState extends State<MapView> {
   //   });
   // }
 
-  Future<Position> CurrentLocation() async {
-    bool serviceEnable;
-    LocationPermission permission;
-    serviceEnable = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnable) {
-      return Future.error('Location services are disabled');
-    }
-    // check if the service location is enabled or not
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permission denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error('Location permission permanently denied');
-    }
-
-    Position position = await Geolocator.getCurrentPosition();
-    return position;
-  }
-
   @override
   Widget build(BuildContext context) {
+    final mapPro = Provider.of<Mapprovider>(context);
     return Scaffold(
       body: GoogleMap(
         myLocationButtonEnabled: false,
         initialCameraPosition: CameraPosition(
-          target: myCurrentLocation,
+          target: mapPro.myCurrentLocation,
           zoom: 14,
         ),
-        markers: marker,
+        markers: mapPro.markers,
         onMapCreated: (GoogleMapController controller) {
-          googleMapController = controller;
+          mapPro.googleMapController = controller;
         },
 
         // markers: {
@@ -88,26 +62,8 @@ class _MapViewState extends State<MapView> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
         onPressed: () async {
-          Position position = await CurrentLocation();
-          googleMapController.animateCamera(
-            CameraUpdate.newCameraPosition(
-              CameraPosition(
-                target: LatLng(position.latitude, position.longitude),
-                zoom: 14,
-              ),
-            ),
-          );
-
-          marker.clear();
-          marker.add(
-            Marker(
-              markerId: MarkerId('This is my current location'),
-              position: LatLng(position.latitude, position.longitude),
-            ),
-          );
-          setState(() {
-            
-          });
+            await mapPro.getCurrentLocation();
+          
         },
         child: Icon(Icons.my_location, size: 30.sp),
       ),
